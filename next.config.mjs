491ece019badffe10f6b1dc174eafe86e5fe29c5
Mst <mst.shahida1980123@gmail.com>
2026-07-20 21:52:19 +0600

    Fix errors and clean up merge conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.imgur.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
     ],
   },
   serverExternalPackages: [
